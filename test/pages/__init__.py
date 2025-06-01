aa158b23b058390ab039d444a33c8c7af89832e8
LEAN-arch <jbautistamsc@gmail.com>
2025-05-31 22:19:38 -0700

    Create __init__.py

---
+++ test/pages/__init__.py
@@ -0,0 +1,4 @@
+# sentinel_project_root/test/pages/__init__.py
+# This file makes 'pages' a Python package, allowing Streamlit to find pages
+# and allowing your pages or app_home to import from modules within `pages`
+# or its sub-packages (like `pages.chw_components_sentinel`).
